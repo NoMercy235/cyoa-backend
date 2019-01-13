032fab55c59238cb4a1aef531bcfa005b7e3686c
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2019-01-13 17:57:58 +0200

    Changed the tag route since it doesn't need to be protected.

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 module.exports = {
     routes: {
         auth: { prefix: '/auth', routes: require('./auth/auth.routes') },
+        tag: { prefix: '/tag', routes: require('./tags/tags.routes') },
         user: { prefix: '/api/user', routes: require('./user/user.routes') },
         collection: { prefix: '/api/collection', routes: require('./collection/collection.routes') },
         story: { prefix: '/api/story', routes: require('./story/story.routes') },
@@ -8,6 +9,5 @@ module.exports = {
         option: { prefix: '/api/option', routes: require('./option/option.routes') },
         attribute: { prefix: '/api/attribute', routes: require('./attribute/attribute.routes') },
         player: { prefix: '/api/player', routes: require('./player/player.routes') },
-        tag: { prefix: '/api/tag', routes: require('./tags/tags.routes') },
     },
 };

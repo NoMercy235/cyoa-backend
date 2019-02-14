250a2f9e947093d91f85461c12f834c8284fb4d4
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2019-02-14 21:34:35 +0200

    The stories now have short and long descriptions.

--- src/models/story.js
+++ src/models/story.js
@@ -9,7 +9,8 @@ const mongoose = require('mongoose');
 const schema = new mongoose.Schema(
     {
         name: { type: String, required: true },
-        description: { type: String, required: true },
+        shortDescription: { type: String, required: true },
+        longDescription: { type: String },
         authorShort: { type: String },
 
         tags: [{ type: String, ref: TAG, required: true }],

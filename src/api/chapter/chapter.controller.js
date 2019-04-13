61c560901a74cb20de03cae26c1a7874b9ee082f
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2019-04-13 17:42:04 +0300

    Update properly handles the case where the chapters change parentChapters.

--- src/api/chapter/chapter.controller.js
+++ src/api/chapter/chapter.controller.js
@@ -14,7 +14,7 @@ chaptersCtrl.callbacks[constants.HTTP_TIMED_EVENTS.BEFORE_CREATE].push((req, ite
 
 chaptersCtrl.callbacks[constants.HTTP_TIMED_EVENTS.AFTER_CREATE].push(async (res, item) => {
     if (item.parentChapter) {
-        const parentChapter = await Chapter.findOne({ _id: item.parentChapter }).exec();
+        const parentChapter = await getChapter(item.parentChapter);
         parentChapter.subChapters.push(item._id);
         await parentChapter.save();
     }
@@ -33,6 +33,22 @@ chaptersCtrl.callbacks[constants.HTTP_TIMED_EVENTS.BEFORE_GET_ONE].push((req, qu
 
 chaptersCtrl.callbacks[constants.HTTP_TIMED_EVENTS.BEFORE_UPDATE].push(async (req, item) => {
     await checkAuthor(req, item);
+
+    // On update, check to see if the parentChapter has changed.
+    const dbChapter = await getChapter(item._id);
+    if (item.parentChapter === dbChapter.parentChapter) return;
+
+    // If it did, remove it from the subChapters list of parentChapter.
+    const oldParentChapter = await getChapter(dbChapter.parentChapter);
+    oldParentChapter.subChapters = oldParentChapter.subChapters.filter(chapterId => {
+        return chapterId.toString() !== item._id;
+    });
+    await oldParentChapter.save();
+
+    // And add it to the new parentChapter's list
+    const newParentChapter = await getChapter(item.parentChapter);
+    newParentChapter.subChapters.push(item._id);
+    await newParentChapter.save();
 });
 
 chaptersCtrl.callbacks[constants.HTTP_TIMED_EVENTS.BEFORE_REMOVE].push(async (req, item) => {
@@ -43,6 +59,10 @@ async function checkAuthor(req, item) {
     if (item.author === req.user._id) {
         throw { message: constants.ERROR_MESSAGES.resourceNotOwned };
     }
+}
+
+async function getChapter(id) {
+    return await Chapter.findOne({ _id: id }).exec();
 }
 
 module.exports = {

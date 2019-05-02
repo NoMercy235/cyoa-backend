71e2002a02b0b3c7e2feebe4ece971f449f091d5
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2019-05-02 18:15:28 +0300

    Added support for HTTPS.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 
 // For debugging
 // mongoose.set('debug', true);
@@ -12,6 +15,7 @@ const config = require('./config');
 const passportConfig = require('./middleware/passport');
 
 const port = process.env.PORT || 8080;
+const httpsPort = process.env.HTTPS_PORT || 443;
 
 // Overriding the deprecated "Promise" module of mongoose.
 // For more information see: https://github.com/Automattic/mongoose/issues/4291
@@ -33,4 +37,25 @@ Object.keys(apiRoutes).forEach((key) => {
     app.use(apiRoutes[key].prefix, apiRoutes[key].routes);
 });
 
-app.listen(port);
+app.listen(port, () => {
+    console.log(`HTTP Server started on port: ${port}`);
+});
+
+// HTTPS server config
+try {
+    const keyPath = path.join(__dirname, '..', 'certs', 'privkey.pem');
+    const certPath = path.join(__dirname, '..', 'certs', 'fullchain.pem');
+    const serverKey = fs.readFileSync(keyPath);
+    const serverCert = fs.readFileSync(certPath);
+
+    const options = {
+        key: serverKey,
+        cert: serverCert,
+    };
+
+    https.createServer(options, app).listen(httpsPort, () => {
+        console.log(`HTTPS Server started on port: ${httpsPort}`);
+    });
+} catch (e) {
+    console.log('No certs for HTTPS');
+}

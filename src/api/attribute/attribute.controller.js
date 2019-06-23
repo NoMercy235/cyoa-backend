1c23c7137921af4e7af2828700f896643b5bd1ad
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2019-06-23 19:23:19 +0300

    fix: updating an attribute's name will forward the change to all the options which have consequences that use that attribute. deleting an attribute will remove the related consequence from every option

--- src/api/attribute/attribute.controller.js
+++ src/api/attribute/attribute.controller.js
@@ -1,6 +1,7 @@
 const BaseController = require('../common/base.controller');
 const Attribute = require('../../models/attribute').model;
 const Story = require('../../models/story').model;
+const Option = require('../../models/option').model;
 const constants = require('../common/constants');
 
 const findByCb = function (req) {
@@ -29,11 +30,39 @@ sequenceCtrl.callbacks[constants.HTTP_TIMED_EVENTS.BEFORE_CREATE].push(async (re
 
 sequenceCtrl.callbacks[constants.HTTP_TIMED_EVENTS.BEFORE_UPDATE].push(async (req) => {
     await checkAuthor(req);
+    const newName = req.body.name;
+
+    await updateOptions(req, (attr, o) => {
+        o.consequences.forEach(con => {
+            if (con.attribute === attr.name) {
+                con.attribute = newName;
+            }
+        });
+        return o.save();
+    });
 });
 
 sequenceCtrl.callbacks[constants.HTTP_TIMED_EVENTS.BEFORE_REMOVE].push(async (req) => {
     await checkAuthor(req);
+    await updateOptions(req, (attr, o) => {
+        o.consequences = o.consequences.filter(con => {
+            return con.attribute !== attr.name;
+        });
+        return o.save();
+    });
 });
+
+async function updateOptions (req, updateCb) {
+    const storyId = req.params.story;
+    const attrId = req.params.id;
+
+    const attr = await Attribute.findOne({ _id: attrId });
+
+    const options = await Option.find({ story: storyId });
+    await Promise.all(
+        options.map(o => updateCb(attr, o)),
+    );
+}
 
 async function checkAuthor(req) {
     const story = await Story.findOne({ _id: req.params.story }).exec();

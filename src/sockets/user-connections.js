1c1a67b239446c85e1108c7b705e30fac46f405e
Alexandru Florian Barascu <alex.florin2352@gmail.com>
2020-02-21 22:02:56 +0200

    feat: only count registered users for online users and don't count them multiple times (NoMercy235/cyoa-frontend#33)

--- src/sockets/user-connections.js
+++ src/sockets/user-connections.js
@@ -1,33 +1,64 @@
 const { SocketEvents } = require('./constants');
 
 const store = {
-    users: {
-        online: 0,
-    },
+    onlineUsers: [],
 };
 
+const getOnlineUsers = () =>
+    store.onlineUsers.filter(({ instances }) => instances).length;
 
-const handleUserConnected = () => {
-    store.users.online ++;
+const findUserIndex = (localId) => {
+    return store.onlineUsers.findIndex(({ id }) => {
+        return id === localId
+    });
+};
+
+const handleUserConnected = (socket) => {
+    const exist = findUserIndex(socket.__INSTANCE_USER_ID);
+    if (exist > -1) {
+        store.onlineUsers[exist].instances ++;
+        socket.emit(
+            SocketEvents.UsersOnline,
+            { onlineUsers: getOnlineUsers() },
+        );
+        return;
+    }
+    store.onlineUsers.push({
+        id: socket.__INSTANCE_USER_ID,
+        instances: 1,
+    });
+    socket.emit(
+        SocketEvents.UsersOnline,
+        { onlineUsers: getOnlineUsers() },
+    );
+    socket.broadcast.emit(
+        SocketEvents.UsersOnline,
+        { onlineUsers: getOnlineUsers() },
+    );
 };
 
 const handleUserDisconnected = socket => () => {
-    store.users.online --;
-    socket.broadcast.emit(
-        SocketEvents.UsersOnline,
-        { onlineUsers: store.users.online },
-    );
+    const exist = findUserIndex(socket.__INSTANCE_USER_ID);
+    if (!store.onlineUsers[exist]) return;
+
+    store.onlineUsers[exist].instances --;
+    if (store.onlineUsers[exist].instances === 0) {
+        // Could also remove the user from the array, if it gets too big
+        socket.broadcast.emit(
+            SocketEvents.UsersOnline,
+            { onlineUsers: getOnlineUsers() },
+        );
+    }
 };
 
 function registerUserConnectionSocket (socket) {
-    handleUserConnected();
+    socket.on(SocketEvents.UserOnline, (id) => {
+        socket.__INSTANCE_USER_ID = id;
+        handleUserConnected(socket);
+    });
     socket.emit(
         SocketEvents.UsersOnline,
-        { onlineUsers: store.users.online },
-    );
-    socket.broadcast.emit(
-        SocketEvents.UsersOnline,
-        { onlineUsers: store.users.online },
+        { onlineUsers: getOnlineUsers() },
     );
 
     socket.on(SocketEvents.Disconnect, handleUserDisconnected(socket));

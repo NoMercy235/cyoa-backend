299f3685c2a0f00cbd1e673c012d93866fc2670b
Alexandru Florian Barascu <alex.florin2352@gmail.com>
2020-03-18 22:50:35 +0200

    fix: onlineUsers are updated on user logout (NoMercy235/cyoa-frontend#50)

--- src/sockets/user-connections.js
+++ src/sockets/user-connections.js
@@ -13,41 +13,43 @@ const findUserIndex = (localId) => {
     });
 };
 
-const handleUserConnected = (socket) => {
-    const exist = findUserIndex(socket.__INSTANCE_USER_ID);
-    if (exist > -1) {
-        store.onlineUsers[exist].instances ++;
-        socket.emit(
-            SocketEvents.UsersOnline,
-            { onlineUsers: getOnlineUsers() },
-        );
-        return;
-    }
-    store.onlineUsers.push({
-        id: socket.__INSTANCE_USER_ID,
-        instances: 1,
-    });
+const sendToSelf = (socket) => {
     socket.emit(
         SocketEvents.UsersOnline,
         { onlineUsers: getOnlineUsers() },
     );
+};
+
+const sendToAll = (socket) => {
+    sendToSelf(socket);
     socket.broadcast.emit(
         SocketEvents.UsersOnline,
         { onlineUsers: getOnlineUsers() },
     );
 };
 
+const handleUserConnected = (socket) => {
+    const exist = findUserIndex(socket.__INSTANCE_USER_ID);
+    if (exist > -1) {
+        store.onlineUsers[exist].instances ++;
+        sendToSelf(socket);
+        return;
+    }
+    store.onlineUsers.push({
+        id: socket.__INSTANCE_USER_ID,
+        instances: 1,
+    });
+    sendToAll(socket);
+};
+
 const handleUserDisconnected = socket => () => {
     const exist = findUserIndex(socket.__INSTANCE_USER_ID);
+    socket.__INSTANCE_USER_ID = undefined;
     if (!store.onlineUsers[exist]) return;
 
     store.onlineUsers[exist].instances --;
     if (store.onlineUsers[exist].instances === 0) {
-        // Could also remove the user from the array, if it gets too big
-        socket.broadcast.emit(
-            SocketEvents.UsersOnline,
-            { onlineUsers: getOnlineUsers() },
-        );
+        sendToAll(socket);
     }
 };
 
@@ -56,10 +58,8 @@ function registerUserConnectionSocket (socket) {
         socket.__INSTANCE_USER_ID = id;
         handleUserConnected(socket);
     });
-    socket.emit(
-        SocketEvents.UsersOnline,
-        { onlineUsers: getOnlineUsers() },
-    );
+    socket.on(SocketEvents.UserOffline, handleUserDisconnected(socket));
+    sendToSelf(socket);
 
     socket.on(SocketEvents.Disconnect, handleUserDisconnected(socket));
 }

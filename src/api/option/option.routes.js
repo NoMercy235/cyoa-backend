4029399721aa5b0a1b4015eba39393f96facf522
Alexandru Florian Barascu <alex.florin2352@gmail.com>
2020-05-20 23:07:40 +0300

    fix: add isOwner middleware for options as well (NoMercy235/cyoa-frontend#80)

--- src/api/option/option.routes.js
+++ src/api/option/option.routes.js
@@ -1,12 +1,28 @@
 const router = require('express').Router();
 const controller = require('./option.controller');
+const { isOwner } = require('../common/base.middleware');
+const Story = require('../../models/story').model;
+const Sequence = require('../../models/sequence').model;
 
-router.post('/many/:sequence', controller.createMany);
-router.get('/story/:story', controller.getAllStoryOptions);
-router.get('/:sequence', controller.get);
-router.post('/:sequence', controller.create);
-router.get('/:sequence/:id', controller.getOne);
-router.put('/:sequence/:id', controller.update);
-router.delete('/:sequence/:id', controller.remove);
+const isStoryOwner = isOwner(
+    Story,
+    (req) => ({ _id: req.params.story }),
+    'author',
+);
+
+const isSequenceOwner = isOwner(
+    Sequence,
+    (req) => ({ _id: req.params.sequence }),
+    'story',
+);
+
+
+router.post('/many/:sequence', isSequenceOwner, controller.createMany);
+router.get('/story/:story', isStoryOwner, controller.getAllStoryOptions);
+router.get('/:sequence', isSequenceOwner, controller.get);
+router.post('/:sequence', isSequenceOwner, controller.create);
+router.get('/:sequence/:id', isSequenceOwner, controller.getOne);
+router.put('/:sequence/:id', isSequenceOwner, controller.update);
+router.delete('/:sequence/:id', isSequenceOwner, controller.remove);
 
 module.exports = router;

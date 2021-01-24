b8aa7e3e02fb256eea61540f2d8eb3cb787a813e
Alexandru Florian Barascu <alexandru.barascu@uipath.com>
2021-01-24 13:17:25 +0200

    feat: enhance Morgan logging (NoMercy235/cyoa-frontend#102)

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const fs = require('fs');
 const path = require('path');
 const io = require('socket.io');
 const http = require('http');
+const chalk = require('chalk');
 
 // For debugging
 // mongoose.set('debug', true);
@@ -40,9 +41,21 @@ mongoose.connect(
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ limit: '400kb' }));
 
+const morganMiddleware = morgan(function (tokens, req, res) {
+    return [
+        chalk.hex('#f78fb3').bold(tokens.date(req, res)),
+        chalk.hex('#ff4757').bold('->'),
+        chalk.hex('#34ace0').bold(tokens.method(req, res)),
+        chalk.hex('#ffb142').bold(tokens.status(req, res)),
+        chalk.hex('#ff5252').bold(tokens.url(req, res)),
+        chalk.yellow(tokens['remote-addr'](req, res)),
+        chalk.hex('#fffa65').bold('from ' + tokens.referrer(req, res)),
+        chalk.hex('#1e90ff')(tokens['user-agent'](req, res)),
+    ].join(' ');
+});
+app.use(morganMiddleware);
 
 app.use(cors());
-app.use(morgan('dev'));
 
 app.use(passportConfig.passport.initialize());
 app.use(passportConfig.prefix, passportConfig.passport.authenticate('jwt', { session: false }));
